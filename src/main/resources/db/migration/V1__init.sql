0340358ad72ba903e9493c6f14628004630302cc
DayDream-21 <vova13158@gmail.com>
2022-02-07 18:52:08 +0300

    Create sql scripts for flyway migration

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,27 @@
+SHOW search_path;
+
+CREATE TABLE users (
+    username varchar(50) NOT NULL,
+    password varchar(100) NOT NULL,
+    enabled boolean NOT NULL,
+    PRIMARY KEY (username)
+);
+
+INSERT INTO users (username, password, enabled)
+VALUES
+       ('user1', '{noop}123', true),
+       ('user2', '{noop}123', true),
+       ('user3', '{noop}123', true);
+
+CREATE TABLE authorities (
+    username varchar(50) NOT NULL,
+    authority varchar(50) NOT NULL,
+    CONSTRAINT authorities_idx UNIQUE (username, authority),
+    CONSTRAINT authorities_idfk_1 FOREIGN KEY (username) REFERENCES users (username)
+);
+
+INSERT INTO authorities (username, authority)
+VALUES
+       ('user1', 'ROLE_ADMIN'),
+       ('user2', 'ROLE_USER'),
+       ('user3', 'ROLE_USER');
