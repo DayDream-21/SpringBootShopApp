89749b9344ca7410bb4449ec0bd9e9fe3f00ab62
DayDream-21 <vova13158@gmail.com>
2022-02-07 20:41:05 +0300

    Create configuration and controller classes

---
+++ src/main/java/com/slavamashkov/springboot_test/configurations/AppConfig.java
@@ -0,0 +1,10 @@
+package com.slavamashkov.springboot_test.configurations;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.slavamashkov.springboot_test")
+public class AppConfig {
+
+}
---
+++ src/main/java/com/slavamashkov/springboot_test/controllers/MainController.java
@@ -0,0 +1,14 @@
+package com.slavamashkov.springboot_test.controllers;
+
+import com.slavamashkov.springboot_test.Cat;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+}
