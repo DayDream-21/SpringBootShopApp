8dec84baa4ea5e72ba5e677c71107e382a10e75b
DayDream-21 <vova13158@gmail.com>
2022-02-09 22:42:40 +0300

    Generate equals and hashCode methods

--- src/main/java/com/slavamashkov/springboot_test/entities/Product.java
+++ src/main/java/com/slavamashkov/springboot_test/entities/Product.java
@@ -2,6 +2,8 @@ package com.slavamashkov.springboot_test.entities;
 
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -11,4 +13,17 @@ public class Product {
     private Long id;
     private String title;
     private int price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && id.equals(product.id) && title.equals(product.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price);
+    }
 }
