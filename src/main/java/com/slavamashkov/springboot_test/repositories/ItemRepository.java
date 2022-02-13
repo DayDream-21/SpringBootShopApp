8b74815e13166e8b7bf309a553de31388328513e
DayDream-21 <vova13158@gmail.com>
2022-02-13 14:08:54 +0300

    Delete useless training files

--- src/main/java/com/slavamashkov/springboot_test/repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.slavamashkov.springboot_test.repositories;
-
-import com.slavamashkov.springboot_test.entities.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
