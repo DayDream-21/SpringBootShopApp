f6bcb904abe118504017b8d6f8a1af93174a165d
DayDream-21 <vova13158@gmail.com>
2022-02-13 21:12:44 +0300

    Create method with filter option
    If parameter word is null then we simply return all products

--- src/main/java/com/slavamashkov/springboot_test/services/ProductService.java
+++ src/main/java/com/slavamashkov/springboot_test/services/ProductService.java
@@ -2,12 +2,14 @@ package com.slavamashkov.springboot_test.services;
 
 import com.slavamashkov.springboot_test.entities.Product;
 import com.slavamashkov.springboot_test.repositories.ProductRepository;
+import com.slavamashkov.springboot_test.specifications.ProductSpecs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -40,5 +42,14 @@ public class ProductService {
 
     public void add(Product product) {
         productRepository.save(product);
+    }
+
+    public List<Product> getProductWithTitleFilter(String word) {
+        Optional<String> optionalTitle = Optional.ofNullable(word);
+        if (optionalTitle.isPresent()) {
+            return productRepository.findAll(ProductSpecs.titleContains(word));
+        } else {
+            return getAllProducts();
+        }
     }
 }
