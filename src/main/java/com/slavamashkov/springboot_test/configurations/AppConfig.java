497cf73ed5161476ae00a05b4378e552ce66187d
DayDream-21 <vova13158@gmail.com>
2022-02-20 20:14:28 +0300

    Enable aspect support in AppConfig

--- src/main/java/com/slavamashkov/springboot_test/configurations/AppConfig.java
+++ src/main/java/com/slavamashkov/springboot_test/configurations/AppConfig.java
@@ -2,8 +2,10 @@ package com.slavamashkov.springboot_test.configurations;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan("com.slavamashkov.springboot_test")
 public class AppConfig {
 
