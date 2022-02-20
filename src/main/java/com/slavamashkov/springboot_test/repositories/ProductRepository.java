8e5562476288c1301dbc027501a72f1522015576
DayDream-21 <vova13158@gmail.com>
2022-02-20 20:18:20 +0300

    Add method to ProductRepository
    Method return page with all products in descending order by views

--- src/main/java/com/slavamashkov/springboot_test/repositories/ProductRepository.java
+++ src/main/java/com/slavamashkov/springboot_test/repositories/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.slavamashkov.springboot_test.repositories;
 
 import com.slavamashkov.springboot_test.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+    @Query("SELECT p FROM Product p ORDER BY p.views DESC")
+    Page<Product> getMostViewedProducts(Pageable pageable);
+
     void deleteProductByTitle(String title);
 }
