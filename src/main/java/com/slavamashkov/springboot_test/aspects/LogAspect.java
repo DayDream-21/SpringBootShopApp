0ac4ffa5cb890a5bc4864c8a6826b4a5737afe1f
DayDream-21 <vova13158@gmail.com>
2022-02-20 20:15:41 +0300

    Create Aspect class
    Maybe do something with logging in the future

---
+++ src/main/java/com/slavamashkov/springboot_test/aspects/LogAspect.java
@@ -0,0 +1,10 @@
+package com.slavamashkov.springboot_test.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+}
